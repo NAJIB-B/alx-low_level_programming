bfd49a076a845c03d570b7bdbafc83767fe25b39
NAJIB-B <najibajibasu@gmail.com>
2023-10-10 13:03:18 -0700

    8-24-hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints fromm 00:00 to 23:59
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; ++i)
+	{
+		for (j = 0; j < 60; ++j)
+		{
+			putchar(((i / 10) % 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(':');
+			putchar(((j / 10) % 10) + '0');
+			putchar((j % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
