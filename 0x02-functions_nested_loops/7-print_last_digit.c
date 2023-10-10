e7b3767ff7aadc3001ce02633caa6cd7e95d77f2
NAJIB-B <najibajibasu@gmail.com>
2023-10-10 12:25:39 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,12 @@ int print_last_digit(int n)
 	int result;
 
 	result = n % 10;
+	if (result < 0)
+	{
+		result *= -1;
+		_putchar(result + '0');
+		return (result);
+	}
 	_putchar(result + '0');
 	return (result);
 }
