14620d0e973de89b1fd58b57da74321d3aa8e71f
NAJIB-B <najibajibasu@gmail.com>
2023-10-11 07:20:49 -0700

    102 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int i, prev, current, tmp;
+	long long int i, prev, current, tmp;
 
 	prev = 1;
 	current = 2;
-	printf("%d, %d, ", prev, current);
-	for (i = 0; i < 50; ++i)
+	printf("%lld, %lld, ", prev, current);
+	for (i = 0; i < 48; ++i)
 	{
-		printf("%d", prev + current);
+		printf("%lld", prev + current);
 		tmp = prev;
 		prev = current;
 		current = tmp + prev;
