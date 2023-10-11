4179b7cc979b45d1d386fe47ce9fd9afe7598f15
NAJIB-B <najibajibasu@gmail.com>
2023-10-11 07:38:44 -0700

    103 fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, prev, current, tmp, even_sum;
+
+	prev = 1;
+	current = 2;
+	even_sum = 2;
+	for (i = 0; i < 30; ++i)
+	{
+		if (((prev + current) % 2) == 0)
+			even_sum += prev + current;
+		tmp = prev;
+		prev = current;
+		current = tmp + prev;
+	}
+	printf("%ld\n");
+	return (0);
+}
