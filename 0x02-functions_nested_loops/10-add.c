9c6ba921b6d8fb34feaa01abf77ae030e2bfa7fc
NAJIB-B <najibajibasu@gmail.com>
2023-10-10 21:54:29 -0700

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two numbers
+ * @p1: first oprand
+ * @p2: second oprand
+ *
+ * Return: returns the sum of the two oprands
+ */
+int add(int p1, int p2)
+{
+	return (p1 + p2);
+}
