07359787f83e5cb41ecc7a3264abc1a4c57c9faf
NAJIB-B <najibajibasu@gmail.com>
2023-10-12 08:58:47 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		if ((i % 5) == 0)
+			printf("Buzz");
+		if ((i % 5) != 0 && (i % 3) != 0)
+			printf("%d", i);
+		putchar(' ');
+	}
+	putchar('\n');
+}
