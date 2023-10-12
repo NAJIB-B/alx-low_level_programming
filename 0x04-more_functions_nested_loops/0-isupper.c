aa19beef9b54f913074da914508cfd3efbdb8bf8
NAJIB-B <najibajibasu@gmail.com>
2023-10-12 02:07:08 -0700

    0 is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - checks if a value if upper  case
+ * @n: parameter to be checked
+ *
+ * Return: 1 if it is upper and 0 otherwise
+ */
+int _isupper(int n)
+{
+	if (n > 64 && n > 91)
+		return (1);
+	else
+		return (0);
+}
