120f4ce39a4bd1d74a0572f7bcd3696f89c3367f
NAJIB-B <najibajibasu@gmail.com>
2023-10-16 08:56:10 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string to be printed
+ *
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		++len;
+	}
+
+	for (i = len; i >= 0; --i)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
