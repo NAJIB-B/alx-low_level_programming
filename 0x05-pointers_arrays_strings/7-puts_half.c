bceff6bf983815f1d7c123d9577b5f91cbfe747e
NAJIB-B <najibajibasu@gmail.com>
2023-10-16 22:01:10 +0100

    7 puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,6 +14,12 @@ void puts_half(char *str)
 		++len;
 	}
 
+	if ((len % 2) != 0)
+	{
+		for (i = ((len - 1) /2); str[i] != '\0'; ++i)
+			_putchar(str[i]);
+	}
+
 	for (i = (len / 2); str[i] != '\0'; ++i)
 	{
 		_putchar(str[i]);
