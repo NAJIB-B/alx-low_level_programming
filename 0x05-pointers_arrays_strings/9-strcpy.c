e3afcdcdb333269018d8d783212069276b643a63
NAJIB-B <najibajibasu@gmail.com>
2023-10-16 23:07:33 +0100

    stycpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,14 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
+	if (src[0])
+	{
 	for (i = 0; src[i] != '\0'; ++i)
 	{
 		dest[i] = src[i];
 	}
 
 	dest[i + 1] = '\0';
-
+	}
 	return (dest);
 }
