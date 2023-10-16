51621faa3166f54b8dddebbc0287a46cd2d092c7
NAJIB-B <najibajibasu@gmail.com>
2023-10-16 09:38:42 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @c: string to be reversed
+ *
+ */
+void rev_string(char *s)
+{
+	int i, len, tmp;
+
+	len = 0;
+	
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		++len;
+	}
+
+	for (i = 0; i >= ((len / 2) -1); --i)
+	{
+		tmp = s[len - (i + 1)];
+		s[len - (i + 1)] = s[i];
+		s[i] = tmp;
+	}
+}
