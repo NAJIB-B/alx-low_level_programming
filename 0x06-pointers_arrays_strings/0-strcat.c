062449df80a91b451229b7d0c46ce3d4b45595bc
NAJIB-B <najibajibasu@gmail.com>
2023-10-18 20:37:24 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+/**
+ * _strcat - concactenates two strings
+ * @dest: destination of the where the other string will be concacted
+ * @src: string that would be concacted
+ *
+ * Return: returns a pointer to the concactenated string i.e dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, len, j;
+
+	j = 0;
+
+	for (i = 0; i != '\0'; ++i)
+		++len;
+	for (i = (len - 1); s[i] != '\0'; ++i)
+	{
+		dest[i] = src[j];
+		++j;
+	}
+
+	return (dest);
+}
