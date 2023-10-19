5764d301b60498bf7af325d0879c8b1cafb51601
NAJIB-B <najibajibasu@gmail.com>
2023-10-19 13:30:30 +0100

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+/**
+ * reverse_array - reverses an array
+ * @a: pointer to the array to be reversed
+ * @n: number of elements in the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < (n / 2); ++i)
+	{
+		tmp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = tmp;
+	}
+}
