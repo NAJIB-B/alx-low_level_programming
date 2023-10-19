611e634646dc6834b5c1b74a5727090ba5804a00
NAJIB-B <najibajibasu@gmail.com>
2023-10-19 13:47:43 +0100

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - convert string to uppercase
+ * @s: string to be converted
+ *
+ * Return: return pointer to the string
+ */
+char *string_toupper(int *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if (s[i] > 96 && s[i] < 123)
+			s[i] -= 32;
+	}
+	return (s);
+}	
