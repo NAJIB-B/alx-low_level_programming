edad333698f7fc86e697e702aa9b1d7c6f5e9fc8
NAJIB-B <najibajibasu@gmail.com>
2023-11-25 08:41:17 +0100

    implemented the puts recursive function fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,8 +10,8 @@ void _puts_recursion(char *s)
 	if (s[0] != '\0')
 	{
 		_putchar(s[0]);
-		return _puts_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 
-	return _putchar('\n');
+	if (s[0] == '\0') _putchar('\n');
 }
