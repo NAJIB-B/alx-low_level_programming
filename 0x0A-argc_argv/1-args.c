8dc5506a1a3a71a4df22656cc63928ded63e49c8
NAJIB-B <najibajibasu@gmail.com>
2023-11-28 10:52:34 +0100

    implemented number of args function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful and non zero otherwise
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+
+}
