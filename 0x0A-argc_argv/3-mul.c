96fb3da5d2f3ecb71e0e640d4c849d226849c448
NAJIB-B <najibajibasu@gmail.com>
2023-11-28 11:13:03 +0100

    implemented multiplying args function

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the result of multiplication of two arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if there are two args and 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 3 || argc < 3)
+	{
+		puts("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
