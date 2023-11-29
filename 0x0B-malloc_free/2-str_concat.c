94214f7d3fa07c5e8909706474f3a54fdf242b1f
NAJIB-B <najibajibasu@gmail.com>
2023-11-29 12:32:51 +0100

    implemented str_concat function fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,24 +17,35 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; ++i)
-		++size;
-
-	for (i = 0; s2[i] != '\0'; ++i)
-		++size;
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; ++i)
+			++size;
+	}
+	if (s2 != NULL)
+	{
+		for (i = 0; s2[i] != '\0'; ++i)
+			++size;
+	}
 	string = malloc(sizeof(char) * (size + 1));
 
 	if (string == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; ++i)
+	if (s1 != NULL)
 	{
-		string[j] = s1[i];
-		++j;
+		for (i = 0; s1[i] != '\0'; ++i)
+		{
+			string[j] = s1[i];
+			++j;
+		}
 	}
-	for (i = 0; s2[i] != '\0'; ++i)
+	if (s2 != NULL)
 	{
-		string[j] = s2[i];
-		++j;
+		for (i = 0; s2[i] != '\0'; ++i)
+		{
+			string[j] = s2[i];
+			++j;
+		}
 	}
 	string[j + 1] = '\0';
 
