add6e5f6458529997eadc35aefd24e3090f8b86d
NAJIB-B <najibajibasu@gmail.com>
2023-11-29 11:48:35 +0100

    implemented strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: returns pointer to the string and NULL if any error
+ * occur
+ */
+char *_strdup(char *str)
+{
+	char *string;
+	int size = 0, i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		++size;
+	}
+
+	string = malloc(sizeof(char) * (size + 1));
+	if (string == NULL)
+		return (NULL);
+
+	for (j = 0; str[j] != '\0'; ++j)
+	{
+		string[j] = str[j];
+	}
+
+	string[j + 1] = '\0';
+
+	return (string);
+}
