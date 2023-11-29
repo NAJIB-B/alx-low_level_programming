8f98a1aceafe5419bf28f8cd4b5d18ce357c92f7
NAJIB-B <najibajibasu@gmail.com>
2023-11-29 12:36:05 +0100

    implemented str_concat function fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,6 @@ char *str_concat(char *s1, char *s2)
 	int i, j = 0, size = 0;
 	char *string;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-
 	if (s1 != NULL)
 	{
 		for (i = 0; s1[i] != '\0'; ++i)
