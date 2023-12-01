9184b98401934fbbdfc27240fcdeac70a8c2a84c
NAJIB-B <najibajibasu@gmail.com>
2023-12-01 15:31:10 +0100

    implemented a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog or null if error occur
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t dog;
+	dog_t *dog_ptr;
+
+	dog_ptr = &dog;
+
+	dog.name = name;
+	dog.age = age;
+	dog.owner = owner;
+
+	return (dog_ptr);
+}
