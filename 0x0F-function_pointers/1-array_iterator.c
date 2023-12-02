dc58381aea7771806bb833bd3669c204cdb127c7
NAJIB-B <najibajibasu@gmail.com>
2023-12-02 10:52:18 +0100

    implemented array iterator function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+/**
+ * array_iterator - iterates over array and performs an action for all the 
+ * elements
+ * @array: the array
+ * @size: size of the array
+ * @action: action to perform on the array
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; ++i)
+	{
+		action(array[i]);
+	}
+}	
