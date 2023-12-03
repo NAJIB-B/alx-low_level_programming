30d60ef0ce908637e2b89d12a0b225482e5bb60b
NAJIB-B <najibajibasu@gmail.com>
2023-12-03 21:48:47 +0100

    implemented a function that prints all arguments (strings)

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings seperated by a separator
+ * @separator: separator to be used
+ * @n: the total count of strings
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+        unsigned int i;
+        char *x;
+
+        va_list args;
+
+        va_start(args, n);
+
+        for (i = 0; i < n; ++i)
+        {
+                x = va_arg(args, char *);
+
+                if (separator == NULL)
+                {
+                        printf("%s", (x == NULL ? "(nil)" : x));
+                }
+                else
+                {
+                        if (i == 0)
+				printf("%s", (x == NULL ? "(nil)" : x));
+                        else
+				printf("%s%s", separator, (x == NULL ? "(nil)" : x));
+                }
+        }
+        printf("\n");
+        va_end(args);
+}
+
+
