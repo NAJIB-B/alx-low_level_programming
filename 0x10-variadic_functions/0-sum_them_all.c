fe94ab2488ff8709be92ee04014d2e466899b142
NAJIB-B <najibajibasu@gmail.com>
2023-12-03 13:59:19 +0100

    implemented function to sum all arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all arguments
+ * @n: number of arguments
+ *
+ * Return: result of addition of arguments or 0 if n is zero
+ */
+int sum_them_all(const unsigned int n)
+{
+	if (n == 0)
+		return (0);
+	va_list args;
+
+	va_start(args, n);
+
+	int i, x, sum = 0;
+
+	for (i = 0; i < n; ++i)
+	{
+		x = va_arg(args, int);
+		sum += x;
+	}
+
+	return (sum);
+}
