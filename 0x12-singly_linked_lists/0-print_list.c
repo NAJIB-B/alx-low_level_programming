c0f71dccd1ce09643bfb62e0d93876c061f2555e
NAJIB-B <najibajibasu@gmail.com>
2023-12-07 21:05:19 +0100

    Implemented function to print linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints a linked list to the console
+ * @h: pointer to linked list
+ *
+ * Return: returns number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (h->next != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			return (1 + print_list(h->next));
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			return (1 + print_list(h->next));
+		}
+
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	return (1);
+}
