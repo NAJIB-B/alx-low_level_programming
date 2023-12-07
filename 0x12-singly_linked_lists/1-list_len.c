53e92704d5ac2bfacf055a03b7aa37b581297783
NAJIB-B <najibajibasu@gmail.com>
2023-12-07 21:15:59 +0100

    Implemented function to print length of linked list fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,19 +4,19 @@
 #include "lists.h"
 
 /**
- * print_len - prints length of a linked list to the console
+ * list_len - prints length of a linked list to the console
  * @h: pointer to linked list
  *
  * Return: returns number of nodes
  */
 
-size_t print_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	if (h == NULL)
 		return (-1);
 	if (h->next != NULL)
 	{
-		return (1 + print_len(h->next));
+		return (1 + list_len(h->next));
 
 	}
 	return (1);
