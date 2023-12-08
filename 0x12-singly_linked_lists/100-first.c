a620c3840b0508fe784c3c4d4af5680a4cc74cbc
NAJIB-B <najibajibasu@gmail.com>
2023-12-08 15:40:26 +0100

    Implemented function to that runs before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_before_main(void) __attribute__((constructor));
+
+/**
+ * print_before_main - prints text before main is executed
+ *
+ */
+
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
