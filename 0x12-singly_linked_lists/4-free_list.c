4a5642cfefaa2409d4190ba08bfbbdca2561cf56
NAJIB-B <najibajibasu@gmail.com>
2023-12-08 15:23:28 +0100

    Implemented function to free space allocated by linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees memory allocated to a list
+ * @head: pointer to the head of the list
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *last_node, *temp;
+
+	last_node = head;
+
+	while (last_node->next != NULL)
+	{
+		temp = last_node->next;
+
+		free(last_node->str);
+		free(last_node);
+
+		last_node = temp;
+	}
+
+	free(last_node->str);
+	free(last_node);
+}
