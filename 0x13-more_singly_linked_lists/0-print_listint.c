ec241a2e5f1b82d9a580c50fc087038b9546d88b
NAJIB-B <najibajibasu@gmail.com>
2023-12-11 08:53:17 +0100

    Implemented function that prints a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_listint - prints all elements of a list
+ * @h: pointer to the list
+ *
+ * Return: returns size of the list or NULL otherwise
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t size = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		++size;
+	}
+	
+	printf("%d\n", h->n);
+	++size;
+
+	return (size);
+}
