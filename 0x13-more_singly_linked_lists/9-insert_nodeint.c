930d5abed4e8db92829ad9b58d8772e6bcf67166
NAJIB-B <najibajibasu@gmail.com>
2023-12-12 10:54:39 +0100

    did task 9 in the more singly linked list project

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 
 /**
+ * listint_len - returns list length of a list
+ * @h: pointer to the list
+ *
+ * Return: returns size of the list or NULL otherwise
+ */
+size_t listint_len(const listint_t *h)
+{
+        size_t size = 0;
+
+        if (h == NULL)
+                return (0);
+
+        while (h->next != NULL)
+        {
+                h = h->next;
+                ++size;
+        }
+
+        ++size;
+
+        return (size);
+}
+
+
+/**
  * insert_nodeint_at_index - inserts a new node at a given index
  * @head: pointer to pointer to the list
  * @idx: index to insert the node
