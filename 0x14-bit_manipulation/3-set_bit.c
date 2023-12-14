2b3392aaaaed50bc1ac31aab52df34b525b0344d
NAJIB-B <najibajibasu@gmail.com>
2023-12-14 21:32:30 +0100

    did task 3 of bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * pow_two - raise two to the power of a number
+ * @n: the exponential of two
+ *
+ * Return: returns result of two to power n
+ */
+unsigned int pow_two(int n)
+{
+        unsigned int result = 1;
+        int i;
+
+        for (i = 0; i < n; ++i)
+        {
+                result *= 2;
+        }
+        return (result);
+}
+
+
+/**
  * set_bit - set the value of bit to 1 at a given index
  * @n: value to be manipulated
  * @index: index of bit to manipulate
@@ -10,8 +29,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n > (sizeof(unsigned long int) * 8))
-		return (-1);i
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
 	*n |= pow_two(index);
 	return (1);
