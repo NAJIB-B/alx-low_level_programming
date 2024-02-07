a52d858ffe0fea94bf46a98adc40d07b6a274bf9
NAJIB-B <najibajibasu@gmail.com>
2024-02-07 09:11:27 +0100

    Implemented function to get the size of a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * dlistint_t - get the size of a list
+ * @h: the list to check the size of
+ *
+ * Return: returns the size of the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+	const dlistint_t *temp;
+
+	if (h == NULL)
+		return (0);
+
+	temp = h;
+
+	while (temp != NULL)
+	{
+		++size;
+	}
+
+	return (size);
+}
